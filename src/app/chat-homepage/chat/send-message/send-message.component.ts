090bc49799c33649219d2ecd55173848cdddde6b
janskiba <49987793+janskiba@users.noreply.github.com>
2021-04-19 13:58:52 +0200

    add form for sending messeges in the template

--- src/app/chat-homepage/chat/send-message/send-message.component.ts
+++ src/app/chat-homepage/chat/send-message/send-message.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-send-message',
   templateUrl: './send-message.component.html',
-  styleUrls: ['./send-message.component.scss']
+  styleUrls: ['./send-message.component.scss'],
 })
 export class SendMessageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit(form: NgForm) {
+    console.log(form.value.message);
+    form.reset();
   }
-
 }
